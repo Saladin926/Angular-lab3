6f943f707758df063e93d0c10c119805d8d674f9
David Ostler <davidtostler@gmail.com>
2017-03-02 00:27:27 -0700

    error checking

--- angular.js
+++ angular.js
@@ -7,6 +7,11 @@ angular.module('tabTagger', [])
   $scope.tags = [{name:"YouTube",color:"red"},{name:"Google Search",color:"cornflowerblue"}];
 
   $scope.addNew = function (url) {
+    if(!url.title || !url.url)
+    {
+      $window.alert("Make Sure You Have A Title And URL");
+      return;
+    }
     $scope.urls.push({ 
       title: url.title,
       url: url.url,
@@ -19,6 +24,11 @@ angular.module('tabTagger', [])
   };
 
   $scope.addNewTag = function (tag) {
+    if(!tag.name)
+    {
+      $window.alert("Make Sure You Have A Name");
+      return;
+    }
     $scope.tags.push({ 
       name: tag.name,
       color: tag.color
