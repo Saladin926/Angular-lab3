f41b0ff3d1f0c75cdacfb4a880500b22107fd2b6
David Ostler <davidtostler@gmail.com>
2017-03-01 23:34:56 -0700

    add tag functionality

--- angular.js
+++ angular.js
@@ -4,17 +4,28 @@ angular.module('tabTagger', [])
 .controller('tabCtrl', ['$scope', function($scope) {
 
   $scope.urls = [];
+  $scope.tags = [{name:"YouTube",color:"red"},{name:"Google Search",color:"cornflowerblue"}];
 
   $scope.addNew = function (url) {
     $scope.urls.push({ 
       title: url.title,
       url: url.url,
-      //tags: url.tags
+      tags: url.tags
     }); 
 
     url.title = '';
     url.url = '';
     url.tags = [];
+  };
+
+  $scope.addNewTag = function (tag) {
+    $scope.tags.push({ 
+      name: tag.name,
+      color: tag.color
+    }); 
+
+    tag.name = '';
+    tag.color = '#ff0000';
   };
 
 }]);
