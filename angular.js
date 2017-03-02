acff464844d610ad5200517567c575ed56b83a28
David Ostler <davidtostler@gmail.com>
2017-03-01 23:08:57 -0700

    added angular.

--- angular.js
+++ angular.js
@@ -3,6 +3,18 @@ angular.module('tabTagger', [])
 
 .controller('tabCtrl', ['$scope', function($scope) {
 
-  
+  $scope.urls = [];
+
+  $scope.addNew = function (url) {
+    $scope.urls.push({ 
+      title: url.title,
+      url: url.url,
+      //tags: url.tags
+    }); 
+
+    url.title = '';
+    url.url = '';
+    url.tags = [];
+  };
 
 }]);
